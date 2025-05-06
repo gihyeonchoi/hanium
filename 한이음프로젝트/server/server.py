195e2a2066018591eec5b5353d876718239b0777
AAA <AAA@DESKTOP-V09D8P9>
2025-05-06 22:36:51 +0900

    ㅁㅁ

--- 한이음프로젝트/server/server.py
+++ 한이음프로젝트/server/server.py
@@ -125,4 +125,4 @@ def analyze():
     return Response(generate(), mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
